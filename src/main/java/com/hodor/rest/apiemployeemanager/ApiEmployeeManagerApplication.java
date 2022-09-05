0f77d394bab0ec5351d26471675a599202fa55dc
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2022-09-05 18:35:13 +0200

    first commit

---
+++ src/main/java/com/hodor/rest/apiemployeemanager/ApiEmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hodor.rest.apiemployeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmployeeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEmployeeManagerApplication.class, args);
+    }
+
+}
